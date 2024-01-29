7dd9b9b82c2935ea4d3469507f9f10b0f2086921
AqsaMunawar <aqsamunawar72@gmail.com>
2024-01-29 21:25:55 +0500

    third commit

--- src/App.tsx
+++ src/App.tsx
@@ -10,56 +10,57 @@ import './App.css';
 function App() {
   return (
     <>
-      <div className='bg-purple-700 pt-10 lg:pt-24  lg:h-96 p-5 min-h-[397px]  '>
+      <div className='bg-purple-700 pt-[56px] lg:pt-[88px]  min-h-[397px] pl-[23px] '>
 
-        <h1 className='text-3xl lg:text-5xl font-extrabold lg:text-center text-white my-5'>
+        <h1 className='text-3xl lg:text-5xl font-extrabold lg:text-center text-[#F7FAFC] leading-[39px]'>
           Simple pricing for your business
         </h1>
-        <p className='lg:text-center text-white text-lg lg:text-2xl font-medium mt-4 '>
+        <p className='lg:text-center text-[#F7FAFC] text-lg lg:text-2xl font-medium mt-4 leading-8 lg:pt-4'>
           Plans that are carefully crafted to suit your business.
         </p>
       </div>
 
       <div className='flex justify-center flex-col lg:flex-row -mt-32 mr-6 ml-5  '>
         {/* Left side box */}
-        <div className='flex flex-col justify-center items-center gap-y-3 lg:rounded-r-none  bg-gray-200 pt-5  text-center  rounded-xl lg:rounded-l-xl shadow-lg lg:min-w-[378px]'>
+        <div className='flex flex-col justify-center items-center shrink-0 lg:rounded-r-none  bg-gray-200 rounded-xl lg:rounded-l-xl shadow-lg lg:min-w-[300px]'>
 
-          <div className='text-2xl font-extrabold text-gray-900'>Premium PRO</div>
-          <div className='text-6xl lg:text-5xl font-extrabold text-gray-900'>$329</div>
-          <div className='text-gray-900 font-medium text-lg lg:mt-8'>billed just once</div>
-          <button className='bg-purple-800  text-base min-w-[250px] lg:px-12 py-2 font-bold text-white rounded-lg m-4'>
+          <div className='text-2xl font-extrabold text-[#171923] leading-8 pt-[35px] '>Premium PRO</div>
+          <div className='text-5xl lg:text-5xl font-extrabold text-[#171923] leading-[48px] pt-4'>$329</div>
+          <div className='text-[#171923] font-medium text-lg lg:pt-8 pt-3 leading-7'>billed just once</div>
+
+          <button className='bg-purple-800 text-center text-base min-w-[250px] lg:px-12 py-2 font-bold text-[#F7FAFC] rounded-lg mt-[32px] mb-[29px]'>
             Get Started
           </button>
         </div>
 
         {/* Right side box */}
-        <div className=' p-11 max-w-[550px]  bg-white shadow-lg  lg:rounded-r-xl '>
-          <p className=' text-gray-900 '>
+        <div className=' min-w-[262px] bg-white shadow-lg  lg:rounded-r-xl lg:min-w-[800px] lg:min-h-[300px] lg:pt-[57px]'>
+          <p className=' text-[#2D3748] pt-[39px] min-w-[220px] pl-2 text-lg lg:pl-12 '>
             Access these features when you get this pricing package for your business.
           </p>
           <div className='flex flex-col'>
-            <div className='flex gap-x-3 items-center my-4'>
-              <img src={TickIcon} alt='tickicon' />
-              <p className='  text-gray-900 mt-2  '>
+            <div className='flex gap-x-3 items-center my-4 '>
+              <img className='pl-3 lg:pl-12' src={TickIcon} alt='tickicon' />
+              <p className='  text-[#2D3748] mt-2 min-w-[28px] text-lg lg:font-normal'>
                 International calling and messaging API
               </p>
             </div>
             
-            <div className='flex gap-x-3 items-center my-4'>
-              <img src={TickIcon} alt='img' />
-              <p className='  text-gray-900 '>
+            <div className='flex gap-x-3 items-center my-4 '>
+              <img className='pl-3 lg:pl-12' src={TickIcon} alt='img'  />
+              <p className='  text-[#2D3748] min-w-[28px] text-lg'>
                 Additional phone numbers
               </p>
             </div>
-            <div className='flex gap-x-3 items-center my-4 '>
-              <img src={TickIcon} alt='img' />
-              <p className='text-gray-900 mt-2'>
+            <div className='flex gap-x-3 items-center my-4  '>
+              <img className='pl-3 lg:pl-12' src={TickIcon} alt='img' />
+              <p className='text-[#2D3748] mt-2 min-w-[28px] text-lg'>
                 Automated messages via Zapier
               </p>
             </div>
             <div className='flex gap-x-3 items-center my-4 '>
-              <img src={TickIcon} alt='img' />
-              <p className='text-gray-900 mt-2'>
+              <img className='pl-3 lg:pl-12' src={TickIcon} alt='img' />
+              <p className='text-[#2D3748] mt-2 min-w-[28px] text-lg'>
                 24/7 support and consulting
               </p>
             </div>
@@ -69,7 +70,7 @@ function App() {
 
       {/* Last box */}
 
-      <div className= 'flex justify-center  items-center flex-col min-h-44 lg:flex-row '>
+      <div className= 'flex justify-center  items-center flex-col min-h-44 lg:flex-row pt-[38px]'>
 
         <div className='flex gap-x-4 py-3 max-w-[300px] items-center justify-center font-bold lg:p-6'>
           <img className='max-w-100 h-auto' src={Icon3} alt="img3" />
